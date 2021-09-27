7367bcb5ac48f4c6faabd1afe938bb0db497b00e
samat <somych1@gmail.com>
2021-09-26 20:53:46 -0500

    first commit

---
+++ src/test/java/com/bc/cloudconfigservice/CloudConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bc.cloudconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
